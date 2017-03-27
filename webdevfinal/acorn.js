486e1f927a503fb2e0559d86e6412358f19c3ae9
mmhennessy <mhennessy@mcad.edu>
2017-03-26 21:10:33 -0500

    Add files via upload
    Commit for Draft

--- webdevfinal/acorn.js
+++ webdevfinal/acorn.js
@@ -1,24 +1,25 @@
-//This was created with the assistance of the fallowing website sources: APO Documentation, W3Schools and Stock Overflow
-
+//This page was created with the assistance of the css tricks website.
 $(document).ready(function() {
   $("a").on('click', function() {
     $(this).css("background-color", "yellow");
   });
 //This makes a yellow background for links when they are clicked on.
 
+$("#slideshow > div:gt(0)").hide();
 
-    $("#slides > div:gt(0)").hide();
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
 
-    setInterval(function() {
-      $('#slides img > div:first')
-      .fadeOut(3000)
-      .next()
-      .fadeIn(3000)
-      .end()
-      .appendTo('#slides')
-    }, 5000);
 //Slideshow of images on home page
 
+
+  $(document).click(function() {
   $('h1').on("dblclick", function() {
     $(this).css("font-size", "300%")
   });
@@ -28,8 +29,17 @@ $(document).ready(function() {
   $('h2').on("dblclick", function() {
     $(this).css("font-size", "200%")
   });
- //Causes sub-headings to enlarge when double clicked. 
 
+   $('p').on("dblclick", function() {
+  $(this).css("font-size", "200%")
+  });
+
+  $('ul').on("dblclick", function() {
+  $(this).css("font-size", "200%")
+  }); 
+ //Causes sub-headings, paragraphs and lists to enlarge when double clicked. 
+
+});  
 });  
 
 
