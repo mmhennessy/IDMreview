2b1d71d86fd2005a8703c8e2b492c03af8b05127
mmhennessy <mhennessy@mcad.edu>
2017-03-27 18:23:49 -0500

    Add files via upload
    Portfolio Draft Commit-3/27/17

---
+++ IDMreview/wdfinal/work.js
@@ -0,0 +1,35 @@
+//This page was created with the assistance of the css tricks website.
+$(document).ready(function() {
+  $("a").on('click', function() {
+    $(this).css("color", "#424ca0");
+  });
+//This makes a light blue color for links when they are clicked on.
+
+
+  $(document).click(function() {
+  $('h1').on("dblclick", function() {
+    $(this).css("font-size", "300%")
+  });
+//Causes headers to enlarge when double clicked.
+
+
+  $('h2').on("dblclick", function() {
+    $(this).css("font-size", "210%")
+  });
+
+   $('h3').on("dblclick", function() {
+    $(this).css("font-size", "180%")
+  });
+
+   $("h2, h3").on('click', function() {
+   $(this).css("color", "#030303");
+  });
+
+   $('p').on("dblclick", function() {
+  $(this).css("font-size", "150%")
+  });
+
+ //Causes sub-headings, paragraphs and lists to enlarge when double clicked. 
+
+});  
+});  
---
+++ IDMreview/webdevfinal/acorn.js
@@ -0,0 +1,45 @@
+//This page was created with the assistance of the css tricks website.
+$(document).ready(function() {
+  $("a").on('click', function() {
+    $(this).css("background-color", "yellow");
+  });
+//This makes a yellow background for links when they are clicked on.
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
+
+//Slideshow of images on home page
+
+
+  $(document).click(function() {
+  $('h1').on("dblclick", function() {
+    $(this).css("font-size", "300%")
+  });
+//Causes headers to enlarge when double clicked.
+
+
+  $('h2').on("dblclick", function() {
+    $(this).css("font-size", "200%")
+  });
+
+   $('p').on("dblclick", function() {
+  $(this).css("font-size", "200%")
+  });
+
+  $('ul').on("dblclick", function() {
+  $(this).css("font-size", "200%")
+  }); 
+ //Causes sub-headings, paragraphs and lists to enlarge when double clicked. 
+
+});  
+});  
+
+
